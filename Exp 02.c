b0ea0f7696db6679f42eef78a47429aa8268c5bd
AkioAsahi06 <akioasahi@proton.me>
2025-11-08 17:09:42 +0530

    Update Exp 02.c

--- Exp 02.c
+++ Exp 02.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main() {
+    int num;
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    if (num > 0) {
+        printf("Positive number\n");
+    } 
+    else if (num < 0) {
+        printf("Negative number\n");
+    } 
+    else {
+        printf("Zero\n");
+    }
+
+    return 0;
+}
