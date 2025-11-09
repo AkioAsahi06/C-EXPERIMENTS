404f5ab1e88758c85b8d015c99d3af4b5554d6ee
AkioAsahi06 <akioasahi@proton.me>
2025-11-09 11:22:25 +0530

    Update Exp 03.c

--- Exp 03.c
+++ Exp 03.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main() {
+    int choice;
+
+    printf("Enter 1 for Addition, 2 for Subtraction: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            printf("You selected Addition\n");
+            break;
+        
+        case 2:
+            printf("You selected Subtraction\n");
+            break;
+        
+        default:
+            printf("Invalid choice\n");
+            break;
+    }
+
+    return 0;
+}
