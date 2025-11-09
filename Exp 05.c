6abe44637c17e49f1886ac867075b114f91f1a01
AkioAsahi06 <akioasahi@proton.me>
2025-11-09 12:35:44 +0530

    Update Exp 05.c

--- Exp 05.c
+++ Exp 05.c
@@ -1,79 +1,110 @@
 #include <stdio.h>
-#define SIZE 100
 
-void sortArray(int arr[], int n) {
-    int i, j, temp;
-    for (i = 0; i < n - 1; i++) {
-        for (j = i + 1; j < n; j++) {
-            if (arr[i] > arr[j]) {
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+int main() {
+    int arr[100], n, i, j, temp, key, found = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Sorting using Bubble Sort
+    for(i = 0; i < n-1; i++) {
+        for(j = 0; j < n-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
 
-    printf("Sorted Array: ");
-    for (i = 0; i < n; i++) {
+    printf("\nSorted array: ");
+    for(i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
-    printf("\n");
-}
 
-void searchArray(int arr[], int n, int key) {
-    int i, found = 0;
-    for (i = 0; i < n; i++) {
-        if (arr[i] == key) {
-            printf("element %d found at position %d\n", key, i + 1);
+    // Searching for an element
+    printf("\n\nEnter element to search: ");
+    scanf("%d", &key);
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            printf("Element %d found at position %d.\n", key, i + 1);
             found = 1;
             break;
         }
     }
 
-    if (!found) {
-        printf("element %d not found\n", key);
-    }
+    if(!found)
+        printf("Element not found in the array.\n");
+
+    return 0;
 }
 
-void matrixAddition(int a[10][10], int b[10][10], int r, int c) {
-    int i, j;
-    printf("Matrix addition:\n");
-    for (i = 0; i < r; i++) {
-        for (j = 0; j < c; j++) {
-            printf("%d ", a[i][j] + b[i][j]);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+
+int main() {
+    int A[10][10], B[10][10], C[10][10];
+    int i, j, k, n;
+
+    printf("Enter size of square matrix (n x n): ");
+    scanf("%d", &n);
+
+    printf("Enter elements of Matrix A:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    printf("Enter elements of Matrix B:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Matrix Addition
+    printf("\nMatrix Addition (A + B):\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+            printf("%d ", C[i][j]);
         }
         printf("\n");
     }
-}
 
-void matrixSubtraction(int a[10][10], int b[10][10], int r, int c) {
-    int i, j;
-    printf("Matrix subtraction:\n");
-    for (i = 0; i < r; i++) {
-        for (j = 0; j < c; j++) {
-            printf("%d ", a[i][j] - b[i][j]);
+    // Matrix Subtraction
+    printf("\nMatrix Subtraction (A - B):\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            C[i][j] = A[i][j] - B[i][j];
+            printf("%d ", C[i][j]);
         }
         printf("\n");
     }
-}
 
-void matrixMultiplication(int a[10][10], int b[10][10], int r1, int c1, int r2, int c2) {
-    if (c1 != r2) {
-        printf("Matrix Multiplication not possible\n");
-        return;
-    }
-
-    int res[10][10] = {0}, i, j, k;
-    printf("Matrix Multiplication:\n");
-
-    for (i = 0; i < r1; i++) {
-        for (j = 0; j < c2; j++) {
-            res[i][j] = 0;
-            for (k = 0; k < c1; k++) {
-                res[i][j] += a[i][k] * b[k][j];
+    // Matrix Multiplication
+    printf("\nMatrix Multiplication (A x B):\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            C[i][j] = 0;
+            for(k = 0; k < n; k++) {
+                C[i][j] += A[i][k] * B[k][j];
             }
-            printf("%d ", res[i][j]);
+            printf("%d ", C[i][j]);
         }
         printf("\n");
     }
+
+    return 0;
 }
