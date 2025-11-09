9d9f4fc5da788ee8a0545f52fee100096d41e272
AkioAsahi06 <akioasahi@proton.me>
2025-11-09 11:38:30 +0530

    Update Exp 04.c

--- Exp 04.c
+++ Exp 04.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main() {
+    int i;
+
+    printf("For loop: ");
+    for (i = 1; i <= 5; i++) {
+        printf("%d ", i);
+    }
+
+    printf("\nWhile loop: ");
+    i = 1;
+    while (i <= 5) {
+        printf("%d ", i);
+        i++;
+    }
+
+    printf("\nDo-while loop: ");
+    i = 1;
+    do {
+        printf("%d ", i);
+        i++;
+    } while (i <= 5);
+
+    return 0;
+}
